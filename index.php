4356ed58d6dade6dd2978e00dc2bb40a5e06925a
kmolema@gmail.com <kmolema@gmail.com>
2018-01-07 00:03:54 +0200

    Fixed style for contributions frame

--- index.php
+++ index.php
@@ -185,7 +185,7 @@
                                 <a class="btn tm-light-blue-bordered-btn tm-news-link" type="submit" onclick="return setDetailsUrl('<?php echo $clubName ?>#memberdetails')">Add Member</a>                                                
                             </div>
 
-                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 tm-news-container flex-order-1">
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 tm-news-container">
                                 <iframe id="memfees" src="frames/clubcontributions.php?club=<?php echo $clubName ?>" style="width:500px;height:150px" frameborder="0" scrolling="no">
                                 </iframe><br><br>
                                 <a class="btn tm-light-blue-bordered-btn tm-news-link" type="submit" onclick="return setDetailsUrl('<?php echo $clubName ?>#contributionsdetails')">Add Contributions</a>                                                
